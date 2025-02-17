fadc079c55603e314bbd39c2766b26b76189f2d8
expy <expy@>
2025-02-17 10:37:56 -0500

    merge with clone and link

--- src/lib/MiscUtils.cpp
+++ src/lib/MiscUtils.cpp
@@ -1,29 +1,17 @@
 #include "MiscUtils.h"
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/Utils/Cloning.h>
+#include <llvm/Linker/Linker.h>
 #include <glog/logging.h>
 
 namespace MiscUtils {
 
 void MergeModules(llvm::Module& M1, const llvm::Module& M2) {
-    // Merge everything from M2 into M1, if there are name conflicts, keep M1's version
-    for (auto& F : M2.functions()) {
-        if (M1.getFunction(F.getName())) {
-            // Function already exists in M1, skip
-            continue;
-        }
-    }
-    for (auto& G : M2.globals()) {
-        if (M1.getGlobalVariable(G.getName())) {
-            // Global already exists in M1, skip
-            continue;
-        }
-    }
-    for (auto& T : M2.aliases()) {
-        if (M1.getNamedAlias(T.getName())) {
-            // Alias already exists in M1, skip
-            continue;
-        }
-    }
+    // Clone M2 to avoid modifying the original
+    auto ClonedM2 = llvm::CloneModule(M2);
+    
+    // Link the modules
+    llvm::Linker::linkModules(M1, std::move(ClonedM2));
 }
 
 void DumpModule(const llvm::Module& M, const std::string& filename) {
